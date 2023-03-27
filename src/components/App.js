ef380c9207b9971d3b1dda7019a6cb6689aca2dd
shiba_1234 <shibajisahu121@gmail.com>
2023-03-28 00:53:50 +0530

    header design done

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,13 @@ import React from "react";
 function App() {
   return (
     <div className="App">
-      <h1>Shibaji</h1>
+      <header>
+        <img
+          src="https://freepngimg.com/thumb/cartoon/4-2-cartoon-transparent-thumb.png"
+          alt="header-img"
+        />
+        <h1>My Contact Lists</h1>
+      </header>
     </div>
   );
 }
