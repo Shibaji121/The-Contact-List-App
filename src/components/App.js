5e06ee3e709547b3b39cb6b7b8ac93b2ad8f7262
shiba_1234 <shibajisahu121@gmail.com>
2023-03-31 01:16:48 +0530

    contact bar style done

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,28 @@ function App() {
         />
         <h1>My Contact Lists</h1>
       </header>
+      <div className="contact-container">
+        <div className="left-container">
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/9408/9408175.png"
+            alt="profile-img"
+          />
+          <div className="user-name">Shibaji Sahu</div>
+          <div className="phone-number">1234567890</div>
+        </div>
+        <div className="right-container">
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/3597/3597075.png"
+            alt="edit-symbol"
+            className="edit-btn"
+          />
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/5676/5676146.png"
+            alt="delete-symbol"
+            className="delete-btn"
+          />
+        </div>
+      </div>
     </div>
   );
 }
